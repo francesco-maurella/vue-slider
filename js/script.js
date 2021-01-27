48247bea8d184dcfa4ba717fce4ab91a17a78c37
Francesco Maurella <francesco.maurella@live.it>
2021-01-27 19:55:53 +0100

    Funzionamento navbar, aggiunta commenti. FINE

--- js/script.js
+++ js/script.js
@@ -1,32 +1,44 @@
 new Vue ({
+  // ELMENTO //
   el : '#app',
 
+  // DATI //
   data : {
-    imgs : [
+    imgs : [ // array di immagini
       '01.jpg',
       '02.jpg',
       '03.jpg',
       '04.jpg'
     ],
-    imgIndex : 0
+    show : 'active', // show, richiama classe active
+    imgIndex : 0 // indexOf di una img (parte dalla prima)
   },
 
+// METODI //
   methods : {
-    next : function(){
-      const limit = this.imgs.length - 1
-      if (this.imgIndex === limit) {
-        this.imgIndex = 0;
-      } else {
-        this.imgIndex++;
+    next : function(){ // Immagine Successiva
+      const limit = this.imgs.length - 1; // ultimo indexOf possibile di una img
+      if (this.imgIndex === limit) { // se l'indexOf(img) è uguale a limit
+        this.imgIndex = 0; // l'indexOf(img) torna alla prima ( = 0)
+      } else { // oppure
+        this.imgIndex++; // aumenta di un numero
       }
     },
-    prev : function(){
-      const limit = this.imgs.length - 1
-      if (this.imgIndex === 0) {
-        this.imgIndex = limit;
-      } else {
-        this.imgIndex--;
+    prev : function(){ // Immagine Precedente
+      const limit = this.imgs.length - 1; // ultimo indexOf possibile di una img
+      if (this.imgIndex === 0) { // se l'indexOf(img) è il primo ( = 0)
+        this.imgIndex = limit; // l'indexOf(img) diventa uguale al limit
+      } else { // oppure
+        this.imgIndex--; // decrementa di un numero
       }
+    },
+    doActive : function(index){ // Rendi Attiva
+      if (index === this.imgIndex) { // se questo index è uguale all'indexOf(img)
+        return this.show; // ritorna il valore dell'oggetto show
+      }
+    },
+    showImg : function(index){ // Mostra Immagine
+      this.imgIndex = index;
     }
   }
 });
