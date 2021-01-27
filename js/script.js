3460b5e0fd24fbdc1e49bbd84b6ee6111dfd7542
Francesco Maurella <francesco.maurella@live.it>
2021-01-27 16:42:06 +0100

    Scorrimento immagini tramite icone frecce

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 new Vue ({
   el : '#app',
+
   data : {
-
+    imgs : [
+      '01.jpg',
+      '02.jpg',
+      '03.jpg',
+      '04.jpg'
+    ],
+    imgIndex : 0
   },
-  methods : {
 
+  methods : {
+    next : function(){
+      const limit = this.imgs.length - 1
+      if (this.imgIndex === limit) {
+        this.imgIndex = 0;
+      } else {
+        this.imgIndex++;
+      }
+    },
+    prev : function(){
+      const limit = this.imgs.length - 1
+      if (this.imgIndex === 0) {
+        this.imgIndex = limit;
+      } else {
+        this.imgIndex--;
+      }
+    }
   }
 });
